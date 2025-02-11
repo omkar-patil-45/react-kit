8042e64ebcec6e348eb6124d54145f155ba0a414
new-college <omkarpatil98986029@gmail.com>
2025-02-11 11:40:19 +0530

    Pass data to eventhandler in Functional Component

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
+// import { useState } from "react";
+
+// const Events = () => {
+//   const [text, setText] = useState("");
+
+//   const handleChange = (event) => {
+//     setText(event.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <input type="text" value={text} onChange={handleChange} />
+//       <p>Typed:{text}</p>
+//     </div>
+//   );
+// };
+
+// export default Events;
+
+//Pass data to the event handler in Functional Component
 
 const Events = () => {
-  const [text, setText] = useState("");
-
-  const handleChange = (event) => {
-    setText(event.target.value);
+  const handleClicke = (id) => {
+    console.log("Clicked Id", id);
   };
-
   return (
     <div>
-      <input type="text" value={text} onChange={handleChange} />
-      <p>Typed:{text}</p>
+      <button onClick={() => handleClicke(1)}>Btn1</button>
+      <button onClick={() => handleClicke(2)}>Btn2</button>
     </div>
   );
 };
