5683a8654d9410f923e3a765c64197ff501d7f92
new-college <omkarpatil98986029@gmail.com>
2025-03-03 13:00:08 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { BrowserRouter as Route, Router, Routes } from "react-router-dom";
 import "./App.css";
-import WillUnmount from "./LifeCycleMethods/WillUnmount";
+import Navbar from "./Router/Navbar";
+import Home from "./Router/Pages/Home";
+import About from "./Router/Pages/About";
+import Contact from "./Router/Pages/Contact";
+// import WillUnmount from "./LifeCycleMethods/WillUnmount";
 // import Mounting from "./LifeCycleMethods/Mounting";
 // import KeysList from "./List&Keys/KeysList";
 // import A from "./A";
@@ -32,7 +37,16 @@ function App() {
 
       {/* <Mounting /> */}
 
-      <WillUnmount />
+      {/* <WillUnmount /> */}
+
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   );
 }
